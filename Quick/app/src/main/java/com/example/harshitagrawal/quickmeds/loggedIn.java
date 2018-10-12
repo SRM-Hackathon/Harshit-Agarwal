66db560248738cf3059d3ca0cad11e7e3ad07366
Harshit Agrawal <44051267+harshitag98@users.noreply.github.com>
2018-10-13 03:27:40 +0530

    profile updated

--- Quick/app/src/main/java/com/example/harshitagrawal/quickmeds/loggedIn.java
+++ Quick/app/src/main/java/com/example/harshitagrawal/quickmeds/loggedIn.java
@@ -1,9 +1,18 @@
 package com.example.harshitagrawal.quickmeds;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class loggedIn extends AppCompatActivity {
+
+    public void profileView(View view)
+    {
+        Intent signUpIntent=new Intent(getApplicationContext(),profile.class);
+        startActivity(signUpIntent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
