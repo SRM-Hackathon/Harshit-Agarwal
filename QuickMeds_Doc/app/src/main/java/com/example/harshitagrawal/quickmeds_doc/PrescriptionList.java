9aa09178dcd81667a8b39a4f55287e5a90ba9612
Ankit Jha <37044020+AnkitJha1998@users.noreply.github.com>
2018-10-13 07:20:45 +0530

    Doctor App Completed

--- QuickMeds_Doc/app/src/main/java/com/example/harshitagrawal/quickmeds_doc/PrescriptionList.java
+++ QuickMeds_Doc/app/src/main/java/com/example/harshitagrawal/quickmeds_doc/PrescriptionList.java
@@ -4,28 +4,68 @@ import java.util.ArrayList;
 
 public class PrescriptionList {
     String docName;
-    ArrayList<String> medicine;
-    ArrayList<String> medQuantity;
+    String med1,med2,med3,med4,med5;
+    String quan1,quan2,quan3,quan4,quan5;
     public PrescriptionList()
     {
 
     }
 
-    public PrescriptionList(String docName, ArrayList<String> medicine, ArrayList<String> medQuantity) {
+    public PrescriptionList(String docName, String med1, String med2, String med3, String med4, String med5, String quan1, String quan2, String quan3, String quan4, String quan5) {
         this.docName = docName;
-        this.medicine = medicine;
-        this.medQuantity = medQuantity;
+        this.med1 = med1;
+        this.med2 = med2;
+        this.med3 = med3;
+        this.med4 = med4;
+        this.med5 = med5;
+        this.quan1 = quan1;
+        this.quan2 = quan2;
+        this.quan3 = quan3;
+        this.quan4 = quan4;
+        this.quan5 = quan5;
     }
 
     public String getDocName() {
         return docName;
     }
 
-    public ArrayList<String> getMedicine() {
-        return medicine;
+    public String getMed1() {
+        return med1;
     }
 
-    public ArrayList<String> getMedQuantity() {
-        return medQuantity;
+    public String getMed2() {
+        return med2;
+    }
+
+    public String getMed3() {
+        return med3;
+    }
+
+    public String getMed4() {
+        return med4;
+    }
+
+    public String getMed5() {
+        return med5;
+    }
+
+    public String getQuan1() {
+        return quan1;
+    }
+
+    public String getQuan2() {
+        return quan2;
+    }
+
+    public String getQuan3() {
+        return quan3;
+    }
+
+    public String getQuan4() {
+        return quan4;
+    }
+
+    public String getQuan5() {
+        return quan5;
     }
 }
--- QuickMeds_Doc/app/src/main/java/com/example/harshitagrawal/quickmeds_doc/WritingPrescriptionActivity.java
+++ QuickMeds_Doc/app/src/main/java/com/example/harshitagrawal/quickmeds_doc/WritingPrescriptionActivity.java
@@ -17,24 +17,27 @@ import java.util.ArrayList;
 public class WritingPrescriptionActivity extends AppCompatActivity {
 
     ListView prescription;
-    ArrayList<String> medicine;
-    ArrayList<String> quantity;
+    String med1,med2,med3,med4,med5;
+    String quan1,quan2,quan3,quan4,quan5;
     ArrayList<String> combined;
 
     String userName,docName;
     EditText med,quan;
 
-
+    int count;
     DatabaseReference prescriptions;
     public void givePrescription(View view)
     {
         String id=prescriptions.push().getKey();
-        PrescriptionList prescriptionList=new PrescriptionList(docName,medicine,quantity);
+        PrescriptionList prescriptionList=new PrescriptionList(docName,med1,med2,med3,med4,med5,quan1,quan2,quan3,quan4,quan5);
         prescriptions.setValue(prescriptionList);
+        Toast.makeText(getApplicationContext(),"Prescription Saved Successfully",Toast.LENGTH_SHORT).show();
+        this.finish();
     }
 
     public void add(View view)
     {
+        if(count>4){Toast.makeText(getApplicationContext(),"You can't add any more medicines as of now",Toast.LENGTH_SHORT).show();return;}
         String medName,medQuan;
         medName=med.getText().toString().trim();
         medQuan=quan.getText().toString().trim();
@@ -43,6 +46,36 @@ public class WritingPrescriptionActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Enter value",Toast.LENGTH_SHORT).show();
             return;
         }
+        switch(count)
+        {
+            case 0:
+                med1=medName;
+                quan1=medQuan;
+                count++;
+                break;
+            case 1:
+                med2=medName;
+                quan2=medQuan;
+                count++;
+                break;
+            case 2:
+                med3=medName;
+                quan3=medQuan;
+                count++;
+                break;
+            case 3:
+                med4=medName;
+                quan4=medQuan;
+                count++;
+                break;
+            case 4:
+                med5=medName;
+                quan5=medQuan;
+                count++;
+                break;
+        }
+
+
         combined.add(medName+",Quantity:"+medQuan);
         ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,combined);
         prescription.setAdapter(arrayAdapter);
@@ -57,11 +90,11 @@ public class WritingPrescriptionActivity extends AppCompatActivity {
         Intent intt=getIntent();
         userName=PrescriptionActivity.USER_NAME;
         docName=PrescriptionActivity.DOC_NAME;
-        medicine=new ArrayList<String>();
-        quantity=new ArrayList<String>();
-        combined=new ArrayList<String>();
+        med1="";med2="";med3="";med4="";med5="";
+        quan1="";quan2="";quan3="";quan4="";quan5="";
         prescriptions= FirebaseDatabase.getInstance().getReference("prescriptions").child(userName);
-
+        combined=new ArrayList<String>();
+        count=0;
         med=findViewById(R.id.medName);
         quan=findViewById(R.id.medQuant);
 
