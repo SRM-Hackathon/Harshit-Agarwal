93d90adb47dd071bf97b1c6f9f755a4f22bb965f
Harshit Agrawal <44051267+harshitag98@users.noreply.github.com>
2018-10-13 12:19:30 +0530

    delivery UI

--- QuickMeds_Delivery/app/src/main/java/com/example/harshitagrawal/quickmeds_delivery/home.java
+++ QuickMeds_Delivery/app/src/main/java/com/example/harshitagrawal/quickmeds_delivery/home.java
@@ -1,9 +1,18 @@
 package com.example.harshitagrawal.quickmeds_delivery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class home extends AppCompatActivity {
+
+    public void viewProfile(View view)
+    {
+        Intent signUpIntent=new Intent(getApplicationContext(),profile.class);
+        startActivity(signUpIntent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
